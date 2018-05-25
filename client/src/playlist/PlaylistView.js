1f4cfce34456aeb51978e1e30cae34e0e3e6dd61
Michal Martinek <martinek8@gmail.com>
2018-05-25 13:55:10 +0200

    add video player

--- client/src/playlist/PlaylistView.js
+++ client/src/playlist/PlaylistView.js
@@ -5,8 +5,15 @@ import { firebaseConnect,  withFirebase, isLoaded, isEmpty } from 'react-redux-f
 import {compose} from 'redux';
 import {connect }from 'react-redux'
 import  search from "youtube-search";
+import YouTube from 'react-youtube';
 
-class PlaylistView extends React.Component<{}> {
+type State = {
+  results: Array<{
+    id: string,
+    title: string,
+  }>
+}
+class PlaylistView extends React.Component<{}, State> {
   state = {
     results: []
   }
@@ -24,9 +31,20 @@ class PlaylistView extends React.Component<{}> {
       this.input.value = ''
     });
   }
+  _onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  }
   render() {
     console.log(this.props)
     const {results} = this.state;
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: { // https://developers.google.com/youtube/player_parameters
+        autoplay: 0
+      }
+    };
     return (
       <div className="App">
         <header className="App-header">
@@ -39,7 +57,14 @@ class PlaylistView extends React.Component<{}> {
           </button>
           {
             this.state.results.map((i) => (
-             <div key={i.id}>{i.title}</div>
+             <div key={i.id}>
+               {i.title}
+               <YouTube
+                videoId={i.id}
+                opts={opts}
+                onReady={this._onReady}
+              />
+             </div>
            ))
           }
         </header>

184673765b4bb5fe083c9aa46f885d2b82cd9f73
Michal Martinek <martinek8@gmail.com>
2018-05-26 23:04:10 +0200

    basic styling menu

--- client/src/app/Menu.js
+++ client/src/app/Menu.js
@@ -3,17 +3,19 @@ import { Link } from "react-router-dom";
 const Menu = () => {
   return (
     <div className="menu">
-      <div className="menu__item">
-        <Link to="/">App</Link>
-      </div>
-      <div className="menu__item">
-        <Link to="/login">Login</Link>
-      </div>
-      <div className="menu__item">
-        <Link to="/settings">Settings</Link>
-      </div>
-      <div className="menu__item">
-        <Link to="/">Playlists</Link>
+      <div className="menu__container">
+        <div className="menu__item">
+          <Link to="/">Disturb</Link>
+        </div>
+        <div className="menu__item">
+          <Link to="/login">Login</Link>
+        </div>
+        <div className="menu__item">
+          <Link to="/settings">Settings</Link>
+        </div>
+        <div className="menu__item">
+          <Link to="/">Playlists</Link>
+        </div>
       </div>
     </div>
   );

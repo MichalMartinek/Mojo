14896827357f98c06e8600b95368fb491f5c4cfe
Michal Martinek <martinek8@gmail.com>
2018-05-29 00:58:48 +0200

    add basic playbar

--- client/src/playlist/Playbar.js
+++ client/src/playlist/Playbar.js
@@ -1,8 +1,19 @@
 /* @flow */
 import React from 'react'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 
 export default () => (
-  <div>
-    <h1>Playbara</h1>
+  <div className="playBar">
+    <div className="playBar__preview">
+
+    </div>
+    <div className="playBar__main">
+      <FontAwesomeIcon icon="backward" className="playBar__icon"/>
+      <FontAwesomeIcon icon="play" className="playBar__icon"/>
+      <FontAwesomeIcon icon="forward" className="playBar__icon"/>
+    </div>
+    <div className="playBar__settings">
+
+    </div>
   </div>
 )
--- client/src/stories/index.js
+++ client/src/stories/index.js
@@ -7,9 +7,13 @@ import { linkTo } from '@storybook/addon-links';
 import {  Welcome } from '@storybook/react/demo';
 import PlayBar from '../playlist/Playbar'
 import '../App.css';
+import '../iconLibrary'
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('PlayBar', module)
-  .add('basic', () => <PlayBar />)
+  .add('basic', () => <div>
+    <h2 style={{marginLeft: '1rem'}}>PlayBar</h2>
+    <PlayBar />
+  </div>)
 ;

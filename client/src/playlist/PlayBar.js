5cc544df4521c734bd6671251d75a1dd075498ab
Michal Martinek <martinek8@gmail.com>
2018-05-29 01:17:29 +0200

    paused props on PlayBar

--- client/src/playlist/PlayBar.js
+++ client/src/playlist/PlayBar.js
@@ -2,14 +2,18 @@
 import React from 'react'
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 
-export default () => (
+type Props = {
+  paused?: boolean,
+};
+
+const PlayBar = (props: Props) => (
   <div className="playBar">
     <div className="playBar__preview">
 
     </div>
     <div className="playBar__main">
       <FontAwesomeIcon icon="backward" className="playBar__icon"/>
-      <FontAwesomeIcon icon="play" className="playBar__icon"/>
+      <FontAwesomeIcon icon={props.paused ? 'play' : 'pause'} className="playBar__icon playBar__icon--main"/>
       <FontAwesomeIcon icon="forward" className="playBar__icon"/>
     </div>
     <div className="playBar__settings">
@@ -17,3 +21,8 @@ export default () => (
     </div>
   </div>
 )
+
+PlayBar.defaultProps = {
+  paused: false,
+};
+export default PlayBar;
--- client/src/stories/index.js
+++ client/src/stories/index.js
@@ -5,15 +5,13 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import {  Welcome } from '@storybook/react/demo';
-import PlayBar from '../playlist/Playbar'
+import PlayBar from '../playlist/PlayBar'
 import '../App.css';
 import '../iconLibrary'
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('PlayBar')} />);
 
 storiesOf('PlayBar', module)
-  .add('basic', () => <div>
-    <h2 style={{marginLeft: '1rem'}}>PlayBar</h2>
-    <PlayBar />
-  </div>)
+  .add('paused', () => <PlayBar paused/>)
+  .add('playing', () => <PlayBar />)
 ;

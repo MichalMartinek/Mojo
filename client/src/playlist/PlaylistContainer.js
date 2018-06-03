044ccf0394325a653573f20f23689c33e4d20ceb
Michal Martinek <martinek8@gmail.com>
2018-06-04 01:53:18 +0200

    basic usage of Playlist

--- client/src/playlist/PlaylistContainer.js
+++ client/src/playlist/PlaylistContainer.js
@@ -1,11 +1,12 @@
 /* @flow */
 
-import React from 'react';
+import React, {Fragment} from 'react';
 import { firebaseConnect } from 'react-redux-firebase'
 import {compose} from 'redux';
 import {connect }from 'react-redux'
 import  search from "youtube-search";
 import PlayBarContainer from './PlayBarContainer'
+import PlaylistComponent from './Playlist'
 import type {Playlist} from "./types";
 
 type Props = {
@@ -32,7 +33,6 @@ class PlaylistContainer extends React.Component<Props, State> {
       maxResults: 10,
       key: 'AIzaSyCA88Ye6O5jP-4DtQz1Ap5SsJ_Z0orYixc',
       type: 'video',
-      part: 'contentDetails'
     };
 
     search(this.input.value, opts, (err, results, pageInfo) => {
@@ -51,45 +51,34 @@ class PlaylistContainer extends React.Component<Props, State> {
     const {results} = this.state
     const playlist = this.props.playlists[this.props.playlistId]
     return (
-      <div className="App">
-
-        <header className="App-header">
-          <h1 className="App-title">Playlist {playlist.title}</h1>
-          <h2>Search</h2>
-          <input type='text' ref={ref => { this.input = ref }} />
-          <button onClick={this.handleSearch}>
-            Search
-          </button>
-          {
-            this.state.results.map((i) => (
-             <div key={i.id}>
-               {i.title}
-               <img src={i.thumbnails.medium.url} />
-               <button onClick={()=>this.handleAdd(i)}>
-                 Add
-               </button>
-             </div>
-           ))
-          }
-        </header>
-        <div>
-          <h2>Videos</h2>
-          { playlist.videos &&
-            Object.keys(playlist.videos).map((key) => {
-              const i = playlist.videos[key]
-              return (
-             <div key={i.id}>
-               {i.title}
-               <img src={i.thumbnails.medium.url} />
-             </div>
-           )})
-          }
+      <Fragment>
+        <div className="playlistContainer">
+          <div className="playlistContainer__playlist">
+            <PlaylistComponent playlist={playlist} itemClick={(e)=>{console.log(e)}} totalTime={{ hours: 3, minutes: 45}}/>
+          </div>
+          <div className="playlistContainer__search">
+            <input type='text' ref={ref => { this.input = ref }} />
+            <button onClick={this.handleSearch}>
+              Search
+            </button>
+            {
+              this.state.results.map((i) => (
+                <div key={i.id}>
+                  {i.title}
+                  <img src={i.thumbnails.medium.url} />
+                  <button onClick={()=>this.handleAdd(i)}>
+                    Add
+                  </button>
+                </div>
+              ))
+            }
+          </div>
         </div>
         <PlayBarContainer
           playlist={playlist}
           node={this.props.firebase.ref().child(`/playlists/${this.props.playlistId}/position`)}
         />
-      </div>
+      </Fragment>
     );
   }
 }

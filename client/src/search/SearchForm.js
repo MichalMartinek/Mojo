da89289b559196919f5a35a4b9b3671c4288664a
Michal Martinek <martinek8@gmail.com>
2018-06-25 14:23:39 +0200

    refactor SearchForm

--- client/src/search/SearchForm.js
+++ client/src/search/SearchForm.js
@@ -5,21 +5,27 @@ import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 type Props = {
   handleForm: (inputText:string, nextPage: ?boolean) => void,
 };
+type State = {
+  inputText: string,
+};
 
-class SearchForm extends React.Component<Props>  {
-  input: ?HTMLInputElement
-
-  handleForm = (e: {preventDefault: ()=>void})=> {
-    e.preventDefault();
-    if (this.input) {
-      this.props.handleForm(this.input.value)
-    }
+class SearchForm extends React.Component<Props, State>  {
+  state = {
+    inputText: '',
+  }
+  handleForm = (event: {preventDefault: ()=>void})=> {
+    event.preventDefault();
+    this.props.handleForm(this.state.inputText)
+  }
+  handleInputChange = (event: {target: {value: string}}) => {
+    this.setState({inputText: event.target.value});
   }
 
   render() {
+    const { inputText } = this.state
     return (
       <form onSubmit={this.handleForm} className="search__form">
-        <input className="search__input" placeholder="Search" type='text' ref={ref => { this.input = ref }} />
+        <input className="search__input" placeholder="Search" value={inputText} onChange={this.handleInputChange}/>
         <button type="submit" className="no-button search__submit">
           <FontAwesomeIcon icon="search"
                            className="search__icon"

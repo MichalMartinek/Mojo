51bf243ce636ff8febc2f03d089a1d91e81bd0b8
Michal Martinek <martinek8@gmail.com>
2018-07-03 23:22:01 +0200

    Add PlaylistForm

---
+++ client/src/common/PlaylistForm.js
@@ -0,0 +1,60 @@
+/* @flow */
+import * as React from 'react';
+
+type Props = {
+  handleForm: (inputText:string, nextPage: ?boolean) => void,
+};
+type State = {
+  inputText: string,
+  validText: boolean,
+  initial: boolean,
+};
+
+class PlaylistForm extends React.Component<Props, State>  {
+  state = {
+    inputText: '',
+    validText: true,
+    initial: true,
+  }
+  handleForm = (event: {preventDefault: ()=>void})=> {
+    event.preventDefault();
+    if (this.state.initial) {
+      this.setState({
+        initial: false
+      })
+    }
+    if (this.validate()) {
+      this.props.handleForm(this.state.inputText)
+    }
+  }
+  handleInputChange = (event: {target: {value: string}}) => {
+    this.setState({inputText: event.target.value});
+    if (!this.state.initial) {
+      this.validate()
+    }
+  }
+  validate = ():boolean => {
+    const validText = this.state.inputText.length > 2
+    this.setState({ validText });
+    return validText
+  }
+
+  render() {
+    const { inputText, validText } = this.state
+    return (
+      <form onSubmit={this.handleForm} className="playlistForm">
+        <input
+          className={`playlistForm__input ${!validText ? 'playlistForm__input--warning': ''}`}
+          placeholder="New playlist"
+          value={inputText}
+          onChange={this.handleInputChange}
+        />
+        <button type="submit" className="no-button playlistForm__submit">
+          Add
+        </button>
+      </form>
+    )
+  }
+}
+
+export default PlaylistForm
---
+++ client/src/profile/types.js
@@ -0,0 +1,21 @@
+// @flow
+
+import type {Playlist} from "../playlist/types";
+
+export type Profile = {
+  isLoaded: string,
+  isEmpty: string,
+  avatarUrl?: string,
+  displayName?: string,
+  email?: string,
+  playlists:{ [string]: boolean }
+};
+
+export type PopulateProfile =  {
+  isLoaded: string,
+  isEmpty: string,
+  avatarUrl?: string,
+  displayName?: string,
+  email?: string,
+  playlists: { [string]: Playlist }
+};

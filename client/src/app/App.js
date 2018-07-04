2d979d5830e0fe56bcec24034ceb480d69a58ac2
Michal Martinek <martinek8@gmail.com>
2018-07-04 12:54:12 +0200

    Menu corresponds to profile state

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -3,29 +3,34 @@
 import React from 'react';
 import { ConnectedRouter } from 'react-router-redux';
 import Menu from './Menu';
-import Home from '../home/HomeView';
-import Profile from '../profile/ProfileView';
-import Login from '../profile/LoginView';
+import HomeView from '../home/HomeView';
+import ProfileView from '../profile/ProfileView';
+import LoginView from '../profile/LoginView';
 import history from '../history';
-import Playlist from '../playlist/PlaylistView';
+import PlaylistView from '../playlist/PlaylistView';
 import NotFound from '../common/NotFoundView';
 import {
   Route,
   Switch,
 } from "react-router-dom";
+import {connect} from "react-redux";
+import {compose} from "redux";
+import {withFirebase, isLoaded, isEmpty} from "react-redux-firebase";
+import type { Profile } from "../profile/types";
 
-class App extends React.Component<{}> {
+class App extends React.Component<{profile: Profile}> {
   render() {
+    const { profile } = this.props
     return (
       <div className="App">
         <ConnectedRouter history={history}>
           <div>
-            <Menu />
+            <Menu loading={!isLoaded(profile)} isAuthenticated={!isEmpty(profile)}/>
             <Switch>
-              <Route exact path="/" component={Home}/>
-              <Route path="/playlist/:id" component={Playlist}/>
-              <Route path="/login" component={Login}/>
-              <Route path="/profile" component={Profile}/>
+              <Route exact path="/" component={HomeView}/>
+              <Route path="/playlist/:id" component={PlaylistView}/>
+              <Route path="/login" component={LoginView}/>
+              <Route path="/profile" component={ProfileView}/>
               <Route component={NotFound}/>
             </Switch>
           </div>
@@ -34,5 +39,11 @@ class App extends React.Component<{}> {
     );
   }
 }
-
-export default App;
+export default compose(
+  withFirebase,
+  connect(
+    (state) => ({
+      profile: state.firebase.profile
+    })
+  )
+)(App)
--- client/src/app/Menu.js
+++ client/src/app/Menu.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
-const Menu = () => {
+type Props = {
+  isAuthenticated: boolean,
+  loading: boolean,
+};
+
+const Menu = ({ isAuthenticated, loading } : Props) => {
+  console.log(loading)
   return (
     <div className="menu">
       <div className="menu__container">
         <div className="menu__item--logo">
           <Link to="/">Mojo</Link>
         </div>
-        <div className="menu__item">
-          <Link to="/login">Login</Link>
-        </div>
-        <div className="menu__item">
-          <Link to="/Profile">Profile</Link>
-        </div>
+        {!loading && (
+        isAuthenticated ?
+          <div className="menu__item">
+            <Link to="/Profile">Profile</Link>
+          </div>
+          :
+          <div className="menu__item">
+            <Link to="/login">Login</Link>
+          </div>
+        )}
+
       </div>
     </div>
   );

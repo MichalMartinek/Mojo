82fe88c319bcd203870d228047834f0a03db8ce6
Michal Martinek <martinek8@gmail.com>
2018-07-07 00:47:40 +0200

    Add login via Github and Facebook, link to new playlist

--- client/src/profile/LoginView.js
+++ client/src/profile/LoginView.js
@@ -4,15 +4,16 @@ import {compose} from 'redux'
 import {connect }from 'react-redux'
 import { withFirebase } from 'react-redux-firebase'
 import { bindActionCreators } from 'redux';
+import { Link } from "react-router-dom";
 import * as actions from "../app/actions";
+import * as firebaseActions from "../common/firebaseActions";
 import {push} from 'react-router-redux'
 import routes from '../app/routes'
+import type {Firebase} from "../common/types";
 
 type Props = {
   push: (path: string) => void,
-  firebase: {
-    login: ({provider: string, type: string}) => void,
-  }
+  firebase: Firebase
 };
 
 class LoginView extends React.Component<Props> {
@@ -32,8 +33,29 @@ class LoginView extends React.Component<Props> {
                 })
                 console.log(res);
                 this.props.push(routes.profile)
-              }}>Login</button>
+              }}>Login with Google</button>
+          <button onClick={async () => {
+            const res = await this.props.firebase.login({
+              provider: 'facebook',
+              type: 'popup',
+              // scopes: ['email'] // not required
+            })
+            console.log(res);
+            this.props.push(routes.profile)
+          }}>Login with Facebook</button>
+          <button onClick={async () => {
+            const res: {additionalUserInfo: { username: string }} = await this.props.firebase.login({
+              provider: 'github',
+              type: 'popup',
+              scope: 'user' // not required
+            })
+            await firebaseActions.updateProfile(this.props.firebase, {displayName: res.additionalUserInfo.username})
+            this.props.push(routes.profile)
+          }}>Login with Github</button>
         </p>
+        <div>
+          Or you can <Link to={routes.newPlaylist}>create new playlist</Link> without login in.
+        </div>
       </div>
     );
   }
